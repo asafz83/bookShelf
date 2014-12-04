442c40dd55fb93762c6b10e6de1c977270af3cb5
asafz83 <asafz83@gmail.com>
2014-12-04 20:30:12 +0200

    Update getBook.js

--- getBook.js
+++ getBook.js
@@ -1,6 +1,17 @@
- 
-var search = location.search;
-search = "{" + search + "}"
-search = search.replace("?", "");
-var searchObject = search.split("&").join(",").split("=").join(":");
-console.log(searchObject);
+function getNameParameter(){
+ var search = location.search;
+ search = search.replace("?", "");
+ var paramsArray = search.split("&");
+ for (var i=0;i<paramsArray.length;i++) {
+   var entryArray = paramsArray[i].split("=");
+   if (entryArray[0] === "name") {
+     return entryArray[1];    
+   }
+ }
+ return "";
+} 
+
+var nameParameter = getNameParameter();
+var listOfBooks = JSON.parse(localStorage.getItem("refael"));
+
+
